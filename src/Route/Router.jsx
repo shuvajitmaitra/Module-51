20fcc1f1ce9b0deb49d3522f82942a363837f26e
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2023-09-30 01:42:20 +0600

    Initial Project setup

---
+++ src/Route/Router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../components/Root/Root";
+import Home from "../components/Home/Home";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+]);
+export default Router;
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,38 @@
+const Register = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="card-body shadow-2xl">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="text"
+            placeholder="email"
+            className="input input-bordered"
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="text"
+            placeholder="password"
+            className="input input-bordered"
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
